31f3e719f010b8369da161a214f5786ad17e01f7
imacbig03 <charlie.cai@g.feitianacademy.org>
2023-04-11 15:42:11 -0400

    Initial Commit

---
+++ Puddy2/Puddy2App.swift
@@ -0,0 +1,17 @@
+//
+//  Puddy2App.swift
+//  Puddy2
+//
+//  Created by iMacBig03 on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Puddy2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
